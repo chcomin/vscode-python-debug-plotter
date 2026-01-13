4886f4b80db1262f93b8ba1949cc2146f7dba67b
Cesar Henrique Comin <chcomin@gmail.com>
2026-01-13 16:00:22 -0300

    improved tests

--- _tests/test_debug.py
+++ _tests/test_debug.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 # Create an image (Gradient)
 x = np.linspace(0, 1, 256)
@@ -11,3 +12,9 @@ theta = np.linspace(0, 2*np.pi, 100)
 points = np.column_stack((np.cos(theta), np.sin(theta)))
 
 print("Breakpoint here")
+
+imgs = np.zeros((8, 584, 565, 3), dtype=np.uint8)
+for idx in range(1, 9):
+    imgs[idx-1] = np.array(Image.open(f"images/2{idx}_training.tif"))
+
+print("Images loaded successfully")
