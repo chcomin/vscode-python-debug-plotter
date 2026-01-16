a57c4d67088d002cf484dabc6a2c41e6e32b7394
Cesar Henrique Comin <chcomin@gmail.com>
2026-01-16 18:59:45 -0300

    Stoped tracking release and fixed plotly dependency

--- src/webview.ts
+++ src/webview.ts
@@ -6,7 +6,7 @@ export function getWebviewContent(webview: vscode.Webview, extensionUri: vscode.
     const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'main.js'));
     const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'styles.css'));
 
-    const plotlyUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'plotly.min.test5.js'));
+    const plotlyUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'plotly.min.js'));
 
     // 'unsafe-eval' and 'worker-src blob:' required for Plotly WebGL
     const csp = `
